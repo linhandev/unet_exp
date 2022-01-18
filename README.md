a65d5ef694f843c7fdbdbd9456ca9e6746483084
Lin Han <me@linhan.email>
2022-01-18 07:43:36 -0500

    prep

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # unet_exp
+hl进展
+
+# 数据集
+- msd
+链接: https://pan.baidu.com/s/13QyioAcNbHCk7QpRJ0B4pA 提取码: a9g2
+--来自百度网盘超级会员v6的分享
+
+- CT spine 1k
+
+# 环境
+```shell
+# 装torch
+conda create -n torch python=3.9
+conda activate torch
+conda update -n base -c defaults conda
+nvcc --version
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+
+# 装nnunet
+git clone https://github.com/MIC-DKFZ/nnUNet.git
+cd nnUNet
+pip install -e .
+pip install --upgrade git+https://github.com/FabianIsensee/hiddenlayer.git@more_plotted_details#egg=hiddenlayer
+pip install matplotlib
+
+# 设置环境变量
+data_dir=/home/lin/Desktop/data/nnunet
+export nnUNet_raw_data_base=${data_dir}
+export nnUNet_preprocessed=${data_dir}/nnUNet_preprocessed
+export RESULTS_FOLDER=${data_dir}/RESULTS_FOLDER
+
+echo  nnUNet_raw_data_base: ${nnUNet_raw_data_base}
+echo  nnUNet_preprocessed: ${nnUNet_preprocessed}
+echo  RESULTS_FOLDER: ${RESULTS_FOLDER}
+```
+
+# nnunet预处理
+
+nnUNet_convert_decathlon_task -i /home/lin/Desktop/msd # 4d序列会拆分成3d
+
+nnUNet_plan_and_preprocess -t 6 -tl 2 -tf 1 --verify_dataset_integrity
---
+++ covpkl.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import os.path as osp
+import pickle
+import json
+
+import numpy as np
+
+
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = "/home/lin/Desktop/data/nnunet/nnUNet_preprocessed/Task006_Lung/"
+print(path)
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """Special json encoder for numpy types"""
+
+    def default(self, obj):
+        if isinstance(
+            obj,
+            (
+                np.int_,
+                np.intc,
+                np.intp,
+                np.int8,
+                np.int16,
+                np.int32,
+                np.int64,
+                np.uint8,
+                np.uint16,
+                np.uint32,
+                np.uint64,
+            ),
+        ):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+        elif isinstance(obj, (np.bool_,)):
+            return bool(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+for root, dirs, files in os.walk(path):
+    for file in files:
+        if file.endswith(".pkl"):
+            print("converting file: ", file)
+            with open(osp.join(root, file), "rb") as f:
+                data = pickle.load(f)
+            # print(type(data))
+            # print(json.dumps(data, cls=NumpyEncoder))
+            with open(osp.join(root, file[:-4] + ".json"), "w") as f:
+                print(json.dumps(data, cls=NumpyEncoder, indent=4), file=f)
